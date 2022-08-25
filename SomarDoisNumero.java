e8dda391a7c7b51b515d7a7f0c7841bf2fc030c2
felipetadeu2002 <103465655+felipetadeu2002@users.noreply.github.com>
2022-08-25 17:18:32 -0300

    Create SomarDoisNumero.java

---
+++ SomarDoisNumero.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+Class SomarDoisNumero {
+  public static Scanner sc = new Scanner(System.in);
+
+  public static void main (String[] args){
+    //Declaracao de variaveis
+	  int num1, num2, soma;
+
+	  //Ler primeira variavel
+	  System.out.println("Digite um numero");
+	  num1 = sc.nextInt();
+
+    //Ler segunda variavel
+	  System.out.println("Digite outro numero");
+	  num2 = sc.nextInt();
+
+    //Somar
+    soma = num1 + num2;
+
+	  //Mostrar na tela
+	  System.out.println("Soma:" + soma);
+  }
+}
