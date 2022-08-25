fbc3d2f752f13731cb1f22ad737e49d33ce17bff
felipetadeu2002 <103465655+felipetadeu2002@users.noreply.github.com>
2022-08-25 17:19:43 -0300

    Update SomarDoisNumero.java

--- SomarDoisNumero.java
+++ SomarDoisNumero.java
@@ -4,21 +4,21 @@ Class SomarDoisNumero {
   public static Scanner sc = new Scanner(System.in);
 
   public static void main (String[] args){
-    //Declaracao de variaveis
-	  int num1, num2, soma;
+	//Declaracao de variaveis
+	int num1, num2, soma;
 
-	  //Ler primeira variavel
-	  System.out.println("Digite um numero");
-	  num1 = sc.nextInt();
+	//Ler primeira variavel
+	System.out.println("Digite um numero");
+	num1 = sc.nextInt();
 
-    //Ler segunda variavel
-	  System.out.println("Digite outro numero");
-	  num2 = sc.nextInt();
+    	//Ler segunda variavel
+	System.out.println("Digite outro numero");
+	num2 = sc.nextInt();
 
-    //Somar
-    soma = num1 + num2;
+    	//Somar
+    	soma = num1 + num2;
 
-	  //Mostrar na tela
-	  System.out.println("Soma:" + soma);
+	//Mostrar na tela
+	System.out.println("Soma:" + soma);
   }
 }
